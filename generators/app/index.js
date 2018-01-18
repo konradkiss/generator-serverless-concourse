f3e864e3cc18830f1956741d9d51c81c81c59746
Konrad Kiss <konrad@konradkiss.com>
2018-01-18 11:51:58 +0100

    Fixing scoping issue

--- generators/app/index.js
+++ generators/app/index.js
@@ -26,18 +26,17 @@ module.exports = class extends Generator {
 
     this.verb = this.options.verb.toUpperCase()
     this.handler = this.endpointHandlerArr[1] || this.options.verb.toLowerCase() + this.fnname.charAt(0).toUpperCase() + this.fnname.slice(1)
-    this.handlerFile = this.handler.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase()
   }
 
   prompting() {
 
     const prompts = [
-      { message: 'API version?', name: 'version', type: 'input', default: this.version },
-      { message: 'Function name', name: 'name', type: 'input', default: this.namePlural },
-      { message: 'HTTP verb', name: 'verb', type: 'input', default: this.verb },
-      { message: 'HTTP path', name: 'path', type: 'input', default: this.namePlural.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase() },
-      { message: 'Handler name', name: 'handler', type: 'input', default: this.handler },
-      { message: 'Enable CORS?', name: 'cors', type: 'confirm', default: false },
+      { message: 'API version?',  name: 'version',  type: 'input',  default: this.version },
+      { message: 'Function name', name: 'name',     type: 'input',  default: this.namePlural },
+      { message: 'HTTP verb',     name: 'verb',     type: 'input',  default: this.verb },
+      { message: 'HTTP path',     name: 'path',     type: 'input',  default: this.namePlural.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase() },
+      { message: 'Handler name',  name: 'handler',  type: 'input',  default: this.handler },
+      { message: 'Enable CORS?',  name: 'cors',   type: 'confirm',  default: false },
     ]
 
     return this.prompt(prompts).then((answers) => {
@@ -45,19 +44,21 @@ module.exports = class extends Generator {
         const today = new Date();
         const day = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
 
+        this.handlerFile = answers.handler.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase()
+
         this.fs.copyTpl(
           this.templatePath('function.ts.txt'),
-          this.destinationPath(`functions/${answers.version}/${answers.name}/${answers.handlerFile}.ts`),
+          this.destinationPath(`functions/${answers.version}/${answers.name}/${this.handlerFile}.ts`),
           { name: answers.name, verb: answers.verb, lverb: answers.verb.toLowerCase(), path: answers.path, handler: answers.handler, cors: answers.cors, username, day, version: answers.version }
         )
 
         this.fs.copyTpl(
           this.templatePath('test.ts.txt'),
-          this.destinationPath(`__tests__/functions/${answers.version}/${answers.name}/${answers.handlerFile}.spec.ts`),
+          this.destinationPath(`__tests__/${answers.version}/${answers.name}/${this.handlerFile}.spec.ts`),
           { name: answers.name, verb: answers.verb, lverb: answers.verb.toLowerCase(), path: answers.path, handler: answers.handler, cors: answers.cors, username, day, version: answers.version }
         )
 
-        const routesText = this.fs.read('routes.yml', { defaults: '' })
+        const routesText = this.fs.read('routes.yml')
         const yamlEdit = YamlEdit(routesText)
         const route = {}
         route[answers.name] = {
